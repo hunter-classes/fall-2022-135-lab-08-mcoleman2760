47649c4712a768fc15edf9d5d93c270fc3b8e267
mcoleman2760 <104951107+mcoleman2760@users.noreply.github.com>
2022-10-31 16:02:05 -0400

    Update funcs.h

--- funcs.h
+++ funcs.h
@@ -2,3 +2,8 @@
 #pragma once
 
 void invert (std::string input);
+void invert_half(std::string input);
+void box(std::string input);
+void frame (std::string input);
+void scale(std::string input);
+void pixelate(std::string input);
